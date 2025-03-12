b6bcb97bc7c7ab84568e04786fc482107d191981
rahulpal388 <rahulschoolmail59@gmail.com>
2025-03-12 09:35:19 +0530

    changing the hhtp server

--- apps/http-server/index.ts
+++ apps/http-server/index.ts
@@ -8,6 +8,9 @@ app.use(express.json())
 app.get("/",(req,res)=>{
     res.status(200).send("http get endpoint")
 })
+app.get("/hello",(req,res)=>{
+    res.status(200).send("hello")
+})
 
 app.post("/signup", async (req, res) => {
     const body = req.body
